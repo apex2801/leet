b94839fa75cc513490a30ca439a8182e756c5076
apex2801 <aapinheiro2005@gmail.com>
2021-03-14 20:31:11 -0300

    .

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Você esperando respostas, olhando pro espaço E eu tão ocupado vivendo, eu não me pergunto, eu faço");
+            Console.WriteLine("Não sei onde eu tô indo, mas sei que eu tô no meu caminho");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("em leet");
+            Console.WriteLine("v0c3 35p3r4nd0 r35p05t45, 0lh4nd0 pr0 35p4ç0 3 3u 740 0cup4d0 v1v3nd0,3u n40 m3 p3rgun70, 3u f4ç0 ");
+            Console.WriteLine("n40 531 0nd3 3u 7o 1ndo, m45 531 qu3 3u n0 m3u c4m1nh0");
+        }
+    }
+}
